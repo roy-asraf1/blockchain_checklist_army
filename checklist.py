588ae694b7d15b9d076bbe3c7a126cd346038766
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2023-08-10 04:22:37 +0300

    Create checklist.py

---
+++ checklist.py
@@ -0,0 +1,16 @@
+
+class Helmet:
+    def __init__(self):
+        self.quantity =0
+        self.collor ="green"
+        self.valid =False
+
+class Uniform:
+    def __init__(self):
+        self.BeltQuantity =0
+        self.shirtQuantity =0
+        self.pantsQuantity =0
+
+class Water_bottle:
+    def __init__(self):
+        self.water_bottleQuantity =0
